81585f068386352fbf365fe5bf09a25c8445ce66
Yaswanthsri.nimmakayala@gmail.com <yaswanth@Yaswanths-MacBook-Pro.local>
2026-03-30 22:50:14 +0530

    Prompt addition

--- model.py
+++ model.py
@@ -10,7 +10,16 @@ model = init_chat_model(
     temperature = 0,
     api_key = os.getenv("API_Key")
 )
-prompt = ''
+prompt = """
+You are an helful SQL agent.
+Rules:
+- You will help the user by taking the user input, and will create SQL queries to relate the user input to answer them.
+- You also follow actions specified by user such as modifications to the data as specified by user.
+- Use the tools to perform actions.  
+- Limit to 5 rows of output values unless explicitly asked.
+- If the tool returns 'Error: ' , revise the SQL and try_again.
+- Prefer explicit column lists; avoid select *.
+"""
 agent = create_agent(
     model = model,
     tools = [],
